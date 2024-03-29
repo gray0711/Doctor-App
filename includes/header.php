5897d54e9449dbcd02f77870aa7efcbea31b860f
Aryan Rangari <80381911+gray0711@users.noreply.github.com>
2024-03-29 15:32:29 +0530

    Add files via upload

--- includes/header.php
+++ includes/header.php
@@ -1,47 +1,95 @@
-<nav class="navbar navbar-expand-lg bg-light fixed-top shadow-lg">
-                <div class="container">
-                    <a class="navbar-brand mx-auto d-lg-none" href="index.php">
-                        Doctor Appointment
-                        <strong class="d-block">Management System</strong>
-                    </a>
 
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
+<nav id="app-navbar" class="navbar navbar-inverse navbar-fixed-top primary">
+  
+  <!-- navbar header -->
+  <div class="navbar-header">
+    <button type="button" id="menubar-toggle-btn" class="navbar-toggle visible-xs-inline-block navbar-toggle-left hamburger hamburger--collapse js-hamburger">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="hamburger-box"><span class="hamburger-inner"></span></span>
+    </button>
 
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav mx-auto">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="index.php">Home</a>
-                            </li>
+    <button type="button" class="navbar-toggle navbar-toggle-right collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="zmdi zmdi-hc-lg zmdi-more"></span>
+    </button>
 
-                            <li class="nav-item">
-                                <a class="nav-link" href="#about">About</a>
-                            </li>
+    <button type="button" class="navbar-toggle navbar-toggle-right collapsed" data-toggle="collapse" data-target="#navbar-search" aria-expanded="false">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="zmdi zmdi-hc-lg zmdi-search"></span>
+    </button>
 
-                           
+    <a href="dashboard.php" class="navbar-brand">
+      <span class="brand-icon"><i class="fa fa-gg"></i></span>
+      <span class="brand-name">DAMS</span>
+    </a>
+  </div><!-- .navbar-header -->
+  
+  <div class="navbar-container container-fluid">
+    <div class="collapse navbar-collapse" id="app-navbar-collapse">
+      <ul class="nav navbar-toolbar navbar-toolbar-left navbar-left">
+        <li class="hidden-float hidden-menubar-top">
+          <a href="javascript:void(0)" role="button" id="menubar-fold-btn" class="hamburger hamburger--arrowalt is-active js-hamburger">
+            <span class="hamburger-box"><span class="hamburger-inner"></span></span>
+          </a>
+        </li>
+        <li>
+          <h5 class="page-title hidden-menubar-top hidden-float">Dashboard</h5>
+        </li>
+      </ul>
 
-                            <a class="navbar-brand d-none d-lg-block" href="index.php">
-                                Doctor Appointment
-                                <strong class="d-block">Management System</strong>
-                            </a>
+      <ul class="nav navbar-toolbar navbar-toolbar-right navbar-right">
+       
 
-                            <li class="nav-item">
-                                <a class="nav-link" href="check-appointment.php">Check Appointment</a>
-                            </li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="zmdi zmdi-hc-lg zmdi-notifications"></i></a>
 
-                            <li class="nav-item">
-                                <a class="nav-link" href="#booking">Booking</a>
-                            </li>
 
-                            <li class="nav-item">
-                                <a class="nav-link" href="#contact">Contact</a>
-                            </li>
-                             <li class="nav-item active">
-                                <a class="nav-link" href="doctor/login.php">Doctor</a>
-                            </li>
-                        </ul>
-                    </div>
 
+          <div class="media-group dropdown-menu animated flipInY">
+         <?php
+         $docid=$_SESSION['damsid'];
+$sql="SELECT * from tblappointment  where Status is null && Doctor=:docid";
+$query = $dbh -> prepare($sql);
+$query-> bindParam(':docid', $docid, PDO::PARAM_STR);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+
+$cnt=1;
+$totalappintments=$query->rowCount();
+foreach($results as $row)
+{ 
+
+  ?>
+
+            <a href="view-appointment-detail.php?editid=<?php echo $row->ID;?>&&aptid=<?php echo $row->AppointmentNumber;?>" class="media-group-item">
+              <div class="media">
+                <div class="media-left">
+                  <div class="avatar avatar-xs avatar-circle">
+                    <img src="assets/images/images.png" alt="">
+                    <i class="status status-online"></i>
+                  </div>
                 </div>
-            </nav>
+                <div class="media-body">
+                  <h5 class="media-heading">New Appointment</h5>
+                  <small class="media-meta"><?php echo $row->AppointmentNumber;?> at (<?php echo $row->ApplyDate;?>)</small>
+                </div>
+              </div>
+            </a><!-- .media-group-item -->
+        <?php  } ?>
+          </div>
+        </li>
+
+        <li class="dropdown">
+          <a href="javascript:void(0)" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="zmdi zmdi-hc-lg zmdi-settings"></i></a>
+          <ul class="dropdown-menu animated flipInY">
+            <li><a href="profile.php"><i class="zmdi m-r-md zmdi-hc-lg zmdi-account-box"></i>My Profile</a></li>
+            <li><a href="change-password.php"><i class="zmdi m-r-md zmdi-hc-lg zmdi-balance-wallet"></i>Change Password</a></li>
+            <li><a href="logout.php"><i class="zmdi m-r-md zmdi-hc-lg zmdi-sign-in"></i>Logout</a></li>
+            
+          </ul>
+        </li>
+
+      </ul>
+    </div>
+  </div><!-- navbar-container -->
+</nav>
